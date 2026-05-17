64807684b4b7be5f7f91f123b3562b1751e082e5
surajdwivedi0307 <surajpratapdwivedi@gmail.com>
2026-05-17 18:35:21 +0530

    Use environment database configuration

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,15 +1,21 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://surajdwivedi@localhost:5432/signalflow"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
 
 Base = declarative_base()
